f3a2ad70efda391b112b0ba7d3d319dc86926352
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-08-01 12:21:05 +0600

    connected to mongodb and hide credentials with dotenv

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const {
+    MongoClient,
+    ServerApiVersion
+} = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +12,33 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dse9fiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+
+        // await client.db("admin").command({
+        //     ping: 1
+        // });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
